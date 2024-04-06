bdd946aadfc3bd81684bd9176b1c925fe385db37
kilbouri <kilbouri@uwindsor.ca>
2024-04-06 03:42:12 -0400

    Floating ground sprite

--- app/src/main/java/project/gameobjects/blocks/FloatingGround.java
+++ app/src/main/java/project/gameobjects/blocks/FloatingGround.java
@@ -1,13 +1,34 @@
 package project.gameobjects.blocks;
 
-import engine.sprites.SpriteSheet;
-import project.PlaceholderSpriteSheet;
+import static project.levels.Level.GRID_SIZE;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import engine.sprites.SpriteUtils;
 
 public class FloatingGround extends Block {
 
-    private static final SpriteSheet sourceSheet = PlaceholderSpriteSheet.getInstance();
+    private static final BufferedImage sourceImage = getSourceSprite();
 
     public FloatingGround(int gridX, int gridY, int gridWidth) {
-        super(sourceSheet.getTile(3), gridX, gridY, gridWidth, 1);
+        super(getScaledSprite(gridWidth, 1), gridX, gridY, gridWidth, 1);
+    }
+
+    private static BufferedImage getSourceSprite() {
+        try {
+            return SpriteUtils.load("sprites/prod/world/floating.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static BufferedImage getScaledSprite(int gridWidth, int gridHeight) {
+        return SpriteUtils.scaleSliced(
+                sourceImage,
+                new Rectangle(32, 1, 32, 30),
+                gridWidth * GRID_SIZE, gridHeight * GRID_SIZE);
     }
 }
